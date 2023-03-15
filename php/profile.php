d71428e6dfc7c95f70fbfcf0a17ae4b2b74f3f0f
Sanjay400 <107196725+Sanjay400@users.noreply.github.com>
2023-03-15 15:44:08 +0530

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -1,11 +1,33 @@
 <?php
+// Connect to MongoDB
+$mongoClient = new MongoDB\Client('mongodb://localhost:27017');
+
+// Select the database and collection
+$database = $mongoClient->selectDatabase('mydatabase');
+$collection = $database->selectCollection('users');
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  
+  // Get form data
   $name = $_POST['name'];
   $email = $_POST['email'];
   $contact = $_POST['contact'];
   $dob = $_POST['dob'];
+  
+
+  
+  
+  $result = $collection->updateOne(
+    ['email' => $email],
+    ['$set' => ['name' => $name, 'contact' => $contact, 'dob' => $dob]]
+  );
+  
+  
   header('Content-Type: application/json');
-  echo json_encode(array('status' => 'success', 'message' => 'Profile updated successfully!'));
+  if ($result->getModifiedCount() == 1) {
+    echo json_encode(array('status' => 'success', 'message' => 'Profile updated successfully!'));
+  } else {
+    echo json_encode(array('status' => 'error', 'message' => 'An error occurred while updating the profile.'));
+  }
 }
-?>
